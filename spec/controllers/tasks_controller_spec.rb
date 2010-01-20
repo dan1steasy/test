769a956ec83810bbda08c7d93ced1267c357c353
Tim Littlemore <tim@tjl2.com>
2010-01-20 15:57:00 +0000

    Started speccing out POST create, but need to sort out making new @task object

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -3,6 +3,11 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe TasksController do
   integrate_views
 
+  def mock_task(stubs={})
+    @mock_task ||= mock_model(Task, stubs)
+  end
+
+
   before(:each) do
     session[:user] = 1
     session[:user_name] = 'admin'
@@ -39,7 +44,11 @@ describe TasksController do
 
   describe "POST create" do
     describe "with valid params" do
-      it "should assign a newly created @task"
+      it "should assign a newly created @task" do
+        post :create,
+          Task.stub(:new).with({'description' => 'task description'}).and_return(mock_task(:save => true))
+        assigns[:task].should equal(mock_task)
+      end
       it "should redirect to show the new @task"
     end
 
