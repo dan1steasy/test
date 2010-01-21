7a6ac26edc450bdbc7eaf98e7b206a4d02f7718b
Tim Littlemore <tim@tjl2.com>
2010-01-21 13:10:01 +0000

    Added webrat requires into spec_helper to allow has_selector to work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'spec/autorun'
 require 'spec/rails'
+require 'webrat'
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
@@ -12,6 +13,7 @@ Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+
 
   # == Fixtures
   #
@@ -44,4 +46,5 @@ Spec::Runner.configure do |config|
   # == Notes
   # 
   # For more information take a look at Spec::Runner::Configuration and Spec::Runner
+  config.include Webrat::Matchers, :type => :views
 end
--- spec/views/tasks/new.html.erb_spec.rb
+++ spec/views/tasks/new.html.erb_spec.rb
@@ -1,21 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 class Task; end
+#session[:user] = 1
+#session[:user_name] = 'admin'
+#session[:list_limit] = 50
 
 describe "/tasks/new" do
   before(:each) do
-    #session[:user] = 1
-    #session[:user_name] = 'admin'
-    #session[:list_limit] = 50
     assigns[:task] = mock_model(Task).as_new_record.as_null_object
     render 'tasks/new'
   end
   
-  it "should have the title 'New Datacentre Task'" do
-    response.should have_tag('title',
-                             'AutoEasy - New Datacentre Task')
-  end
-
   it "should render a form to create a task" do
     response.should have_selector('form', :method => 'post',
                                   :action => tasks_path) do |form|
