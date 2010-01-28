1eb9098ca381a1de29236778d3ce09382bdda9ab
Tim Littlemore <tim@tjl2.com>
2010-01-28 18:40:40 +0000

    Still trying to spec new.html.erb. Fucking gay waste of time!

--- spec/views/tasks/show.html.erb_spec.rb
+++ spec/views/tasks/show.html.erb_spec.rb
@@ -4,7 +4,7 @@ class Task; end
 
 describe "/tasks/show.html.erb" do
   before(:each) do
-    assigns[:task] = stub_model(Task,
+    assigns[:task] = stub_model(Task, :id => 1,
       :description => "Description of this task",
       :is_complete => false,
       :created_by => 1,
@@ -16,4 +16,27 @@ describe "/tasks/show.html.erb" do
   it "should render the description in a <p>" do
     response.should have_tag("p", "Description of this task")
   end
+
+  it "should render the is_complete attribute in a checkbox" do
+    response.should have_tag("input", :type => "checkbox", 
+                             :id => "task_is_complete")
+  end
+
+  it "should provide a link to edit the task" do
+    pending
+    #response.should have_tag("a[href~=/.*\/tasks\/1\/edit$/]", "Edit task")
+    #response.should have_selector('a', :href => /.*\/tasks\/1\/edit$/,
+    #                              :content => "Edit task")
+  end
+
+  it "should provide a link to delete the task" do
+    pending
+    response.should have_tag("a[href=/tasks/1$]", "Delete task", :method => "delete",
+                             :confirm => "Are you sure you want to delete this task?")
+  end
+
+  it "should show who created the task"
+
+  it "should show who updated the task"
+
 end
