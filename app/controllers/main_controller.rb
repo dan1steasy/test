0aa277713d7ac429c3f5e7bfcc299d1605bf5b2b
Tim Littlemore <tim@tjl2.com>
2010-02-08 16:45:15 +0000

    Incomplete tasks now viewable from main page. Giving up on adding rspec for now so will merge and delete this branch.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,9 @@ class MainController < ApplicationController
   def index
     @latest_accounts = Account.find(:all, :order => 'created_at DESC', :limit => 12)
     @latest_companies = Company.find(:all, :order => 'created_at DESC', :limit => 12)
+    @incomplete_tasks = Task.find(:all, :order => 'created_at ASC',
+                                  :conditions => ['is_complete = ?', false])
+    #@notes = Note.find_with_recent_activity
   end
 
 end
